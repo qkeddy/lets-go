bab37bcf75d83d6dfdfe7d3ad576b5b6a64beffe
Quin Eddy <qkeddy@gmail.com>
2022-05-23 16:14:41 -0400

    Updated `resolvers` and `typeDefs` so the server  would start

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,7 +6,50 @@ const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 
 const resolvers = {
+    Query: {
+        // Find a single or multiple users
+        users: async (parent, { _id }) => {
+            const params = _id ? { _id } : {};
+            return await User.find(params).populate("savedActivities");
+        },
 
+        // Get the profile of the logged in user and populate savedBooks
+        me: async (parent, args, context, xxx) => {
+            console.log({ parent, args, context, xxx });
+            if (context.user) {
+                return await User.findOne({ _id: context.user._id }).populate("savedActivities");
+            }
+            throw new AuthenticationError("You need to be logged in!");
+        },
+    },
+
+    Mutation: {
+        login: async (parent, { username, email, password }) => {
+            const user = await User.findOne({ $or: [{ username }, { email }] });
+
+            if (!user) {
+                throw new AuthenticationError("Can't find this user");
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError("Wrong password!");
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        // Create a new user based upon 3 required fields and return the user obj and token
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+
+ 
+    },
 };
 
 module.exports = resolvers;
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,7 +3,34 @@ const { gql } = require("apollo-server-express");
 
 // Setup type definitions
 const typeDefs = gql`
+    type User {
+        _id: ID!
+        username: String!
+        email: String!
+        password: String!
+    }
 
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        # Supports query of one or multiple users
+        users(_id: String): [User]
+
+        # A query for the logged in user of "User" type and return only selected fields.
+        me: User
+    }
+
+    type Mutation {
+        # Create a user with username, email, password all required
+        createUser(username: String!, email: String!, password: String!): Auth
+
+        # Login user with the option to enter a username or email and require the password
+        login(username: String, email: String, password: String!): Auth
+
+    }
 `;
 
 module.exports = typeDefs;

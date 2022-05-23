032f6f69876966398f698631fe9703beb762a8c2
Quin Eddy <qkeddy@gmail.com>
2022-05-23 15:43:16 -0400

    Added boilerplate schema structure

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,12 @@
+// Import Signing & Authentication libraries
+const { signToken } = require("../utils/auth");
+const { AuthenticationError } = require("apollo-server-express");
+
+// Import Mongoose data model
+const { User } = require("../models");
+
+const resolvers = {
+
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,9 @@
+// Import Apollo GraphQL
+const { gql } = require("apollo-server-express");
+
+// Setup type definitions
+const typeDefs = gql`
+
+`;
+
+module.exports = typeDefs;

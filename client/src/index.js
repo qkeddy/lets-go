08adb432d76f434640d0d677fc5932fe9c2067ec
Isaak Morales <ice.isaak97@gmail.com>
2022-05-24 11:29:38 -0400

    making the header and footer stay consistant throughout the page

--- client/src/index.js
+++ client/src/index.js
@@ -6,16 +6,23 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom"
+import Header from './components/header'
+import Footer from './components/footer'
 
 // ReactDOM.render takes a React component, or tree of React components and (eventually) renders them to the DOM.
 // The first argument is the component we want to render (<App/>), and second is the target element to render to (#root)
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
+root.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <Header />
+        <App />
+        <Footer />
+      </BrowserRouter>
+    </React.StrictMode>,
+  );
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

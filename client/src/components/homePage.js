46f07d9bc2f7f563b4a5670134e947bb62824a7e
Dom <Domalbano35@gmail.com>
2022-05-24 14:03:31 -0400

    added button to homepage

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -1,14 +1,65 @@
 import React from 'react'
+import ButtonUnstyled, { buttonUnstyledClasses } from '@mui/base/ButtonUnstyled';
+import { styled } from '@mui/system';
+import Stack from '@mui/material/Stack';
+
+const blue = {
+  500: '#007FFF',
+  600: '#0072E5',
+  700: '#0059B2',
+};
+
+const CustomButton = styled(ButtonUnstyled)`
+  font-family: IBM Plex Sans, sans-serif;
+  font-weight: bold;
+  font-size: 0.875rem;
+  background-color: ${blue[500]};
+  padding: 12px 24px;
+  border-radius: 8px;
+  color: white;
+  transition: all 150ms ease;
+  cursor: pointer;
+  border: none;
+
+  &:hover {
+    background-color: ${blue[600]};
+  }
+
+  &.${buttonUnstyledClasses.active} {
+    background-color: ${blue[700]};
+  }
+
+  &.${buttonUnstyledClasses.focusVisible} {
+    box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1), 0 0 0 5px rgba(0, 127, 255, 0.5);
+    outline: none;
+  }
+
+  &.${buttonUnstyledClasses.disabled} {
+    opacity: 0.5;
+    cursor: not-allowed;
+  }
+`;
+
+
 
 export default function Home() {
   return (
     <div className="App">
       <header className="App-header">
         <main>
-          <h1>hello</h1>
+          <h1>Hello! Welcome to Let's Go!</h1>
+
+          <Stack spacing={2} direction="row">
+
+            <CustomButton>Button</CustomButton>
+
+          </Stack>
         </main>
       </header>
     </div>
   );
-  
+
 }
+
+
+

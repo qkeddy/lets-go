3a9cddb322b2b227c18fe8b3905cd7b3e18c034b
Dom <Domalbano35@gmail.com>
2022-05-24 14:52:53 -0400

    fixed footer to bottom of the screen and added minimal styles

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./footer.css"
 
 export default function Footer() {
   return (
-    <div className="App">
+    <div className="App footer">
       <header className="App-header">
         <main>
           <h1>you can see me</h1>

1d40cdcabc2bcaae4f8dac79cd5c57b68915648d
Dom <Domalbano35@gmail.com>
2022-05-24 13:16:00 -0400

    wrote minimal code in each component to make sure it was making contact and render

---
+++ client/src/components/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <main>
+          <h1>present</h1>
+        </main>
+      </header>
+    </div>
+  );
+}

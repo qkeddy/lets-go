24a1a712a8b53814c561174e217aa7a50a27b0ad
Julian Richardson <jrichar3@yahoo.com>
2022-05-25 11:34:06 -0400

    created the activities.js file

---
+++ server/models/Activities.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require("mongoose");
+
+const activitySchema = new Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        location: {
+            type: String,
+            required: true,
+        },
+        lng: {
+            type: Number,
+        },
+        lat: {
+            type: Number,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        participants: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ]
+    }
+)
+
+
+const Activity = model("Activity", activitySchema);
+
+module.exports = Activity;
+
+// name: String!
+// location: String!
+// lng: Int
+// lat: Int
+// description: String!
+// participants: [User]
+

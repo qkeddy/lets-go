cfdcc19cea7522790e93e0f567d767c370490e8c
Dom <Domalbano35@gmail.com>
2022-05-25 23:26:59 -0400

    added files to client utils folder

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,60 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_USER = gql`
+  mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// export const ADD_ACTIVITY = gql`
+//   mutation addActivity(
+//     $bookId: String!
+//     $authors: [String]!
+//     $description: String
+//     $image: String
+//     $link: String
+//     $title: String!
+//   ) {
+//     saveBook(
+//       bookId: $bookId
+//       authors: $authors
+//       description: $description
+//       image: $image
+//       link: $link
+//       title: $title
+//     ) {
+//       # The fields below are not used, but syntactically required.
+//       username
+//       savedBooks {
+//         bookId
+//         authors
+//         description
+//         image
+//         link
+//         title
+//       }
+//     }
+//   }
+// `;
+
+// export const REMOVE_ACTIVITY = gql`
+//   mutation deleteBook($bookId: String!) {
+//     deleteBook(bookId: $bookId) {
+//       username
+//       savedBooks {
+//         bookId
+//         authors
+//         description
+//         image
+//         link
+//         title
+//       }
+//     }
+//   }
+// `;

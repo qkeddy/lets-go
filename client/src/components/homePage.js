abdbb471b597e105e048918461f602b5664e7fee
Dom <Domalbano35@gmail.com>
2022-05-26 12:49:00 -0400

    added card to homepage

--- client/src/components/homePage.js
+++ client/src/components/homePage.js
@@ -4,7 +4,12 @@ import { style, styled } from '@mui/system';
 import TextField from "@mui/material/TextField";
 // import Stack from "@mui/material/Stack";
 import Autocomplete from "@mui/material/Autocomplete";
-
+import Box from "@mui/material/Box";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 const blue = {
   500: '#007FFF',
@@ -66,6 +71,15 @@ const eventStyle ={
   width: '24%'
 }
 
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
+  >
+    •
+  </Box>
+);
+
 
 
 export default function Home() {
@@ -121,6 +135,41 @@ export default function Home() {
           </form>
 
           <CustomButton>Search</CustomButton>
+
+          <container>
+            <Card
+              sx={{
+                border: "10px solid black",
+                width: "25%",
+                display: "block",
+              }}
+            >
+              <CardContent>
+                <Typography
+                  sx={{ fontSize: 14 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="div">
+                  be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </container>
+          
         </main>
       </header>
     </div>
@@ -136,7 +185,7 @@ const eventList = [
 ];
 
 const locationList = [
-  { location: "Seatle" },
+  { location: "Seattle" },
   { location: "Philadelphia" },
   { location: "Washington D.C." },
   { location: "New York" },

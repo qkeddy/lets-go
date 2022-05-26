10caee4847b22f7d288c6462a4eb646217993413
Dom <Domalbano35@gmail.com>
2022-05-26 00:34:08 -0400

    added functionality to login modal

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,17 @@
 import { gql } from "@apollo/client";
 
+export const LOGIN_USER = gql`
+  mutation login($username: String, $email: String, $password: String!) {
+    login(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
 export const CREATE_USER = gql`
   mutation createUser($username: String!, $email: String!, $password: String!) {
     createUser(username: $username, email: $email, password: $password) {
